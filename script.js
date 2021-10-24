c4db3258cd1a3f1a63afed857169d1a9c422c5e7
yoshida-github <82887464+yoshida-github@users.noreply.github.com>
2021-10-24 17:19:39 +0900

    スムーススクロールの実装と微修正

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 'use strict'
 
 $(function () {
+
+    // スムーススクロール
+    $('a[href^="#"]').click(function () {
+        let speed = 500;
+        let href = $(this).attr("href")
+        /* もしhrefの値が # または "" のとき html を target に代入
+        そうでない場合、href を target に代入 */
+        let target;
+        if (href === "#" || href === "") {
+            target = $(html);
+        } else {
+            target = $(href);
+        }
+        let header = $('header').height();
+        let position = target.offset().top - header;
+        $("html, body").animate({ scrollTop: position }, speed, "swing");
+        return false;
+    });
+
     // メニュー画面切り替えファンクション
     function toggleMenu() {
         $('.main-nav').toggleClass('show')
