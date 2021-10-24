4c19ac175f1c53acf302668af2fc9b0584e3ab8a
yoshida-github <82887464+yoshida-github@users.noreply.github.com>
2021-10-24 16:54:27 +0900

    メニューボタンのアニメーションを実装

--- script.js
+++ script.js
@@ -3,10 +3,13 @@
 $(function () {
     // メニュー画面切り替えファンクション
     function toggleMenu() {
-        $('.main-nav').toggleClass('show');
+        $('.main-nav').toggleClass('show')
+        $('.line1').toggleClass('line1-rotate')
+        $('.line2').toggleClass('line2-rotate');
+        $('.line3').toggleClass('line3-rotate');
     };
     // ボタンがクリックされたとき
-    $('.menu-btn, .main-nav').on('click', function () {
+    $('.menu-btn, .main-nav a').on('click', function () {
         toggleMenu()
     });
 });
